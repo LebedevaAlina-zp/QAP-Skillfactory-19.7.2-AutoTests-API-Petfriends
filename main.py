c7797e65d0cf1f65dbbf3e899660f4cb957a59b0
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2022-12-14 02:55:38 +0400

    Created project copied from https://github.com/SkillfactoryCoding/QAP_PetFriensTesting

---
+++ main.py
@@ -0,0 +1,28 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+
+my_pf = PetFriends()
+key = my_pf.get_api_key(valid_email, valid_password)
+print(key)
+
+pets_list = my_pf.get_list_of_pets(key[1], "my_pets")
+print(pets_list)
+
+
+new_pets_name = "Tom"
+new_pets_type = "Mr. Cat"
+new_pets_age = "1"
+new_pets_photo = "tests/images/cat1.jpg"
+new_pet = my_pf.add_new_pet(key[1], new_pets_name, new_pets_type, new_pets_age, new_pets_photo)
+print(new_pet)
+pet_id = new_pet[1]['id']
+
+upd_name = "Sailor"
+upd_type = "cat"
+upd_age = "4"
+upd_pet = my_pf.update_pet_info(key[1], pet_id, upd_name, upd_type, upd_age)
+print(upd_pet)
+
+del_pet = my_pf.delete_pet(key[1], pet_id)
+print(del_pet)
+
---
+++ settings.py
@@ -0,0 +1,3 @@
+
+valid_email = "lebedeva-zp@yandex.ru"
+valid_password = "Grusha"

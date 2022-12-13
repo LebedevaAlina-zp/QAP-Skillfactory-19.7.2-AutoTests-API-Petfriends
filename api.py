31050d006f17f19c061e8b6a09052975cac5cc9d
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2022-12-14 03:21:34 +0400

    Two methods added to PetFriends class

--- api.py
+++ api.py
@@ -71,7 +71,6 @@ class PetFriends:
         return status, result
 
 
-
     def delete_pet(self, auth_key: json, pet_id: str) -> json:
         """Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
         статус запроса и результат в формате JSON с текстом уведомления о успешном удалении.
@@ -107,4 +106,48 @@ class PetFriends:
             result = res.json()
         except json.decoder.JSONDecodeError:
             result = res.text
+        return status, result
+
+
+    def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце (без фото) и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
+
+
+    def add_pets_photo(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + f'api/pets/set_photo/{pet_id}', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
         return status, result
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ print(key)
 pets_list = my_pf.get_list_of_pets(key[1], "my_pets")
 print(pets_list)
 
-
 new_pets_name = "Tom"
 new_pets_type = "Mr. Cat"
 new_pets_age = "1"
@@ -26,3 +25,16 @@ print(upd_pet)
 del_pet = my_pf.delete_pet(key[1], pet_id)
 print(del_pet)
 
+new_pets_name = "Jack"
+new_pets_type = "Mr. Dog"
+new_pets_age = "3"
+new_simple_pet = my_pf.add_new_pet_without_photo(key[1], new_pets_name, new_pets_type, new_pets_age)
+print(new_simple_pet)
+pet_id = new_simple_pet[1]['id']
+
+pet_photo = "tests/images/cat1.jpg"
+pet_with_photo = my_pf.add_pets_photo(key[1], pet_id, pet_photo)
+print(pet_with_photo)
+
+del_pet = my_pf.delete_pet(key[1], pet_id)
+print(del_pet)
